a4b6ac8ea55461860ff0772e0d5d2d2c382fe443
soloking <soloking.x@gmail.com>
2020-11-14 19:33:41 +0800

    Update config and disable ssh

--- custom.sh
+++ custom.sh
@@ -4,3 +4,6 @@ echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.d
 
 # Change default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+
+# Copy .config to openwrt
+cp ../.config .config
