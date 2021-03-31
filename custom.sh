daed152ea0e98fad7701ce6a78d2957cf016ece3
xsoloking <soloking.x@gmail.com>
2021-03-31 22:32:52 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -3,7 +3,7 @@ sed -i 's/^#//g' feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 
 # Change default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.99/g' package/base-files/files/bin/config_generate
 
 # Copy .config to openwrt
 cp ../.config .config
